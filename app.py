82e21e3f63a3aa16e221c57a9c09cfdb234d039c
Jon Bramley <jon.bramley@sainsburys.co.uk>
2020-05-04 17:18:39 +0100

    Try  wtihout  ssl

--- app.py
+++ app.py
@@ -221,5 +221,6 @@ def allowed_file(filename):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, ssl_context="adhoc", host="0.0.0.0", port=port)
+    # app.run(debug=True, ssl_context="adhoc", host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
 
