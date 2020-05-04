e4302fb5399f7aef9dff11c31377abb11fac051b
Jon Bramley <jon.bramley@sainsburys.co.uk>
2020-05-04 17:42:45 +0100

    fix

--- notify.py
+++ notify.py
@@ -12,5 +12,5 @@ def send(file, email):
         template_id=template_id, # required UUID string
         personalisation={'link_to_file': prepare_upload(file)}
     )
-    logging.info(response.__dict__)
+    __import__('pprint').pprint(response)
     logging.info("done sending email notification")
