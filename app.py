d838486bd1fb5ced4da155569ae018c65099a01c
Jon Bramley <jon.bramley@sainsburys.co.uk>
2020-05-04 15:51:46 +0100

    logging

--- app.py
+++ app.py
@@ -78,6 +78,8 @@ def login():
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
+    logging.info("--->/login request.base_url: %s", request.base_url)
+
     # Use library to construct the request for Google login and provide
     # scopes that let you retrieve user's profile from Google
     request_uri = client.prepare_request_uri(
