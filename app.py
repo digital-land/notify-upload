a067fdac0aec091dfaaf84f3619f5913cca8c965
Jake Mulley <jakemulley@users.noreply.github.com>
2020-06-09 13:35:55 +0100

    Remove host binding

--- app.py
+++ app.py
@@ -206,4 +206,4 @@ def allowed_file(filename):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port, ssl_context="adhoc")
+    app.run(debug=True, port=port, ssl_context="adhoc")
